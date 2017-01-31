8c399dfeb8b5879fd2e1202898dc38951af69463
nstark <reynz11@gmail.com>
2017-01-31 16:57:34 -0500

    Removed extra lines
    Removed extra SELECT lines, and deleted obsolete assembly lines from JOIN ON

--- GX3Test.sql
+++ GX3Test.sql
@@ -1,16 +1,11 @@
 SELECT DISTINCT
     wsg.schedule_group_name sg,
-    -- MAX(date_time_completion) OVER (partition BY wsg.schedule_group_name) last_scan,
-    -- MAX(model_number) OVER (partition BY wsg.schedule_group_name) current_model,
-    -- MAX(date_time_completion) OVER (partition BY wsg.schedule_group_name, we.wip_entity_name) last_time_by_model,
     MAX(model_number) OVER (partition BY wsg.schedule_group_name, we.wip_entity_name) sched_model,
     MAX(we.wip_entity_name) OVER (partition BY wsg.schedule_group_name, we.wip_entity_name) wo,
     MAX(wdj.build_sequence) OVER (partition BY wsg.schedule_group_name, we.wip_entity_name) bs,
     MAX(wdj.scheduled_start_date) OVER (partition BY wsg.schedule_group_name, we.wip_entity_name) sched,
     MAX(wdj.start_quantity) OVER (partition BY wsg.schedule_group_name, we.wip_entity_name) ord_qty,
     MAX(wdj.quantity_completed) OVER (partition BY wsg.schedule_group_name, we.wip_entity_name) bf_d
-   -- SUM(case when BP.PRODUCTION_LINE IN ('RES1', 'SPL1', 'COM1','HSS','HSG','HSE') THEN 1 ELSE 0 END) OVER (partition BY wsg.schedule_group_name, we.wip_entity_name) scanned,
-    --SUM(case when BP.PRODUCTION_LINE IN ('RES1', 'SPL1', 'COM1','HSS','HSG','HSE') THEN 1 ELSE 0 END) OVER (partition BY wsg.schedule_group_name order by wdj.build_sequence range unbounded preceding) day_scanned
 
 FROM
     bwc_production bp
@@ -25,7 +20,7 @@ ON
 JOIN wip.wip_schedule_groups wsg
 ON
     wsg.schedule_group_id = wdj.schedule_group_id
-    AND wsg.schedule_group_name IN ('COMMASSY', 'FINAL ASSY', 'SPECIAL ASSY','HS GAS','HS ELEC','SPEC')
+    AND wsg.schedule_group_name IN ('COMMASSY', 'HS GAS','HS ELEC','SPEC')
 WHERE
     bp.date_time_completion BETWEEN sysdate - 1 AND sysdate
     AND bp.production_line IN ('RES1', 'SPL1', 'COM1', 'REP', 'WIP', 'PEND','HSS','HSE','HSG')
@@ -35,17 +30,13 @@ UNION
 
 SELECT
     wsg.schedule_group_name sg,
---    NULL,
---    NULL,
---    NULL,
     msib.segment1,
     we.wip_entity_name,
     wdj.build_sequence,
     wdj.scheduled_start_date,
     wdj.start_quantity ord_qty,
     wdj.quantity_completed
-    --0,
-    --0
+
 FROM
     wip.wip_discrete_jobs wdj
 JOIN wip.wip_entities we
@@ -54,7 +45,7 @@ ON
 JOIN wip.wip_schedule_groups wsg
 ON
     wsg.schedule_group_id = wdj.schedule_group_id
-    AND wsg.schedule_group_name IN ('FINAL ASSY', 'SPECIAL ASSY', 'COMMASSY','SPEC','HS GAS','HS ELEC')
+    AND wsg.schedule_group_name IN ('COMMASSY','SPEC','HS GAS','HS ELEC')
 JOIN inv.mtl_system_items_b msib
 ON
     msib.organization_id = 101
@@ -66,5 +57,4 @@ WHERE
     AND wdj.status_type = 3 OR wdj.status_type = 6
 ORDER BY
     sg,
-    --last_time_by_model nulls last,
     bs;
