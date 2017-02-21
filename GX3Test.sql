ded5900eac97d10895c31230162096cacf0e50ff
nstark <reynz11@gmail.com>
2017-02-21 17:11:28 -0500

    Add CASE Statement
    Add CASE statement. Get rid of bwc.production table, replace with wip_move_transactions

--- GX3Test.sql
+++ GX3Test.sql
@@ -1,4 +1,9 @@
 SELECT DISTINCT
+    CASE
+      WHEN TO_CHAR(wdj.scheduled_start_date, 'D') IN(2, 3) THEN 'MONDAY_SCHEDULE'
+      WHEN TO_CHAR(wdj.scheduled_start_date, 'D') IN(4, 5, 6) THEN 'WEDNESDAY_SCHEDULE'
+      ELSE 'WEEKEND_SCHEDULE'
+    END
     wsg.schedule_group_name sched_group,
     MAX(model_number) OVER (partition BY wsg.schedule_group_name, we.wip_entity_name) sched_model,
     MAX(we.wip_entity_name) OVER (partition BY wsg.schedule_group_name, we.wip_entity_name) wo,
@@ -8,11 +13,11 @@ SELECT DISTINCT
     MAX(wdj.quantity_completed) OVER (partition BY wsg.schedule_group_name, we.wip_entity_name) quantity_completed
 
 FROM
-    bwc_production bp
+    wip.wip_move_transactions wmt
 JOIN wip.wip_entities we
 ON
-    we.wip_entity_name = bp.work_order_number
-    AND we.organization_id = bp.organization_id
+    we.wip_entity_name = wmt.wip_entity_id
+    AND we.organization_id = wmt.organization_id
     AND we.organization_id = 101
 JOIN wip.wip_discrete_jobs wdj
 ON
@@ -25,7 +30,7 @@ WHERE
     --bp.date_time_completion BETWEEN sysdate - 1 AND sysdate
     wdj.scheduled_start_date <= NEXT_DAY(TRUNC(sysdate), 'MONDAY') - 5                                                    -- Get everything starting from last Monday and previous (add Wednesday?)
     AND bp.production_line IN ('COM1', 'HSS','HSE','HSG')                                                                 -- Are there additional lines to monitor?
-    AND bp.organization_id = 101
+    AND wmt.organization_id = 101
     AND wdj.quantity_completed != wdj.start_quantity                                                                      -- Get anything that isn't complete
     AND wdj.status_type = 3 OR wdj.status_type = 6                                                                        -- status_type 3 = Released and 6 = On Hold
 
