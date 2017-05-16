48c8ceb770a9ee0f917e4631f45747f2da6f32dd
nstark <reynz11@gmail.com>
2017-05-16 10:40:11 -0400

    Testing for completion

--- GX3PartTwo.sql
+++ GX3PartTwo.sql
@@ -1,18 +1,16 @@
 SELECT DISTINCT
     wsg.schedule_group_name sched_group,
-    SUM(WMT.TRANSACTION_QUANTITY) OVER (partition by WSG.SCHEDULE_GROUP_NAME) COMPLETED
+    SUM(WMT.TRANSACTION_QUANTITY) OVER (PARTITION BY WSG.SCHEDULE_GROUP_NAME) COMPLETED
 FROM WIP_MOVE_TRANSACTIONS WMT
 
 JOIN wip_discrete_jobs wdj
-ON
-  wdj.wip_entity_id = wmt.wip_entity_id
-  AND  wdj.organization_id = wmt.organization_id
-  AND wdj.organization_id = 101
-JOIN
-  wip_schedule_groups wsg ON wsg.organization_id = wdj.organization_id
-  AND wsg.schedule_group_id = wdj.schedule_group_id
-  AND wsg.schedule_group_name IN ('COMMASSY', 'HS GAS','HS ELEC','SPEC')
+    ON wdj.wip_entity_id = wmt.wip_entity_id
+    AND  wdj.organization_id = wmt.organization_id
+    AND wdj.organization_id = 101
+JOIN wip_schedule_groups wsg ON wsg.organization_id = wdj.organization_id
+    AND wsg.schedule_group_id = wdj.schedule_group_id
+    AND wsg.schedule_group_name IN ('COMMASSY', 'HS GAS','HS ELEC','SPEC')
 WHERE
    wmt.organization_id = 101
-   AND wmt.TRANSACTION_DATE between TRUNC(sysdate -1) + 3/24 and TRUNC(sysdate) + 3/24
+   AND wmt.TRANSACTION_DATE BETWEEN TRUNC(sysdate -1) + 3/24 AND TRUNC(sysdate) + 3/24
    ORDER BY 1;
--- GX3Test.sql
+++ GX3Test.sql
@@ -12,19 +12,16 @@ SELECT DISTINCT
 FROM
     wip.wip_move_transactions wmt
 JOIN wip.wip_entities we
-ON
-    we.wip_entity_name = wmt.wip_entity_id
+    ON we.wip_entity_name = wmt.wip_entity_id
     AND we.organization_id = wmt.organization_id
     AND we.organization_id = 101
 JOIN wip.wip_discrete_jobs wdj
-ON
-    wdj.wip_entity_id = we.wip_entity_id
+    ON wdj.wip_entity_id = we.wip_entity_id
 JOIN wip.wip_schedule_groups wsg
-ON
-    wsg.schedule_group_id = wdj.schedule_group_id
+    ON wsg.schedule_group_id = wdj.schedule_group_id
     AND wsg.schedule_group_name IN ('COMMASSY', 'HS GAS','HS ELEC','SPEC')
 WHERE
-    wdj.scheduled_start_date >= NEXT_DAY(TRUNC(sysdate), 'MONDAY') - 7
+    wdj.scheduled_start_date >= NEXT_DAY(TRUNC(sysdate), 'MONDAY') - 21
     AND wmt.organization_id = 101
     AND wdj.quantity_completed != wdj.start_quantity                                                -- Get anything that isn't complete
     AND wdj.status_type IN (3, 6)                                                                   -- status_type 3 = Released and 6 = On Hold
